02d7f82e7c0fd8710d46bc890a655dac37235201
benjih <benji@sixp.in>
2026-06-06 17:50:00 +0100

    chore: add some basic performance test

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,9 @@
 import * as vscode from "vscode"
-import { initDebugOutput } from "./debug/metrics"
+import {
+	clearRecordedMetrics,
+	getRecordedMetrics,
+	initDebugOutput,
+} from "./debug/metrics"
 import { SearchPanel } from "./search/searchPanel"
 
 export function activate(context: vscode.ExtensionContext) {
@@ -10,8 +14,18 @@ export function activate(context: vscode.ExtensionContext) {
 			SearchPanel.show(context)
 		},
 	)
+	const getDebugMetrics = vscode.commands.registerCommand(
+		"fullTabSearch.getDebugMetrics",
+		() => getRecordedMetrics(),
+	)
+	const clearDebugMetrics = vscode.commands.registerCommand(
+		"fullTabSearch.clearDebugMetrics",
+		() => {
+			clearRecordedMetrics()
+		},
+	)
 
-	context.subscriptions.push(openSearch)
+	context.subscriptions.push(openSearch, getDebugMetrics, clearDebugMetrics)
 }
 
 export function deactivate() {}
